fd333ffcd625918149869a3a3dc8b608c1f8e664
vishu7im <vishu.im.0007@gmail.com>
2022-12-18 17:19:43 +0530

    f

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,8 @@
 // api
 import express from "express";
-import { user_create } from "../controller/auth.js";
+import { login, user_create } from "../controller/auth.js";
 
 export const auth = express.Router();
 
 auth.post("/create", user_create);
+auth.post("/login", login);
